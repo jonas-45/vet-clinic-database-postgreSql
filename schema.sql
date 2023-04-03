0b64a06ea6b9255918a932c4efc614969c975b63
jonas-45 <jonaswullah@gmail.com>
2023-04-03 11:02:50 +0000

    Insert values into animal table with sql command

--- schema.sql
+++ schema.sql
@@ -8,3 +8,10 @@ CREATE TABLE animals (
   neutered boolean, 
   weight_kg decimal(6,2)
 );
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES 
+('Agumon','2020-02-03',0,true,10.23),
+('Gabumon','2018-11-15',2,true,8),
+('Pikachu','2021-01-07',1,false,15.04),
+('Devimon','2017-05-12',5,true,11);
