efa62ff18a40de3146112f9d0ec9b418f1ac2896
jonas-45 <jonaswullah@gmail.com>
2023-04-03 20:09:42 +0000

    Add sql query to select neutered animals with less than 3 escapte attempts

--- queries.sql
+++ queries.sql
@@ -3,3 +3,5 @@
 SELECT * FROM animals WHERE name like '%mon'
 
 SELECT name FROM animals WHERE date_part('year', date_of_birth) >= 2016 and date_part('year', date_of_birth) <= 2019
+
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3
