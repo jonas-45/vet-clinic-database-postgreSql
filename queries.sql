cdcb24778feb90b1aec14e3c947caba4b0b4581f
jonas-45 <jonaswullah@gmail.com>
2023-04-03 11:52:34 +0000

    Add sql query to select animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name like '%mon'
+
+SELECT name FROM animals WHERE date_part('year', date_of_birth) >= 2016 and date_part('year', date_of_birth) <= 2019
