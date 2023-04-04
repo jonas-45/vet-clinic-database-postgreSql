0441ee2787fd0ef5ca862da9dfb4cb44820fd4cf
jonas-45 <jonaswullah@gmail.com>
2023-04-04 22:26:57 +0000

    Add transaction verification queries

--- queries.sql
+++ queries.sql
@@ -19,16 +19,21 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3
 /* Project DAY 2 queries */
 BEGIN;
 UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals
+
 ROLLBACK;
+SELECT * FROM animals
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
+SELECT * FROM animals
 
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
+SELECT * FROM animals
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
