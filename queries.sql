bdc302c6a567a3e99a8c77e78362752a14994054
jonas-45 <jonaswullah@gmail.com>
2023-04-05 01:30:04 +0000

    Add verification queries

--- queries.sql
+++ queries.sql
@@ -40,8 +40,11 @@ DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT SP1;
 UPDATE animals SET weight_kg = weight_kg * (-1);
 ROLLBACK TO SP1;
+SELECT * FROM animals
+
 UPDATE animals SET weight_kg = weight_kg * (-1) WHERE weight_kg < 0;
 COMMIT;
+SELECT * FROM animals
 
 SELECT count(*) FROM animals /*How many animals are there */
 
