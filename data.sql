f0340cbadd9353916878f33b27c359c58d3c1692
jonas-45 <jonaswullah@gmail.com>
2023-04-05 15:11:17 +0000

    Update owner_id in the animals table

--- data.sql
+++ data.sql
@@ -40,3 +40,9 @@ INSERT INTO species (name) VALUES
 
 UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
 UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander','Squirtle','Blossom');
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
