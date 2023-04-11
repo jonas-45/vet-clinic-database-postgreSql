09f3c5dfe207cd29723a790a85a670bfddafbaf1
jonas-45 <jonaswullah@gmail.com>
2023-04-11 09:28:34 +0000

    Add queries to select data from visits and owners tables

--- queries.sql
+++ queries.sql
@@ -140,3 +140,7 @@ WHERE sp.id IS NULL
 /* What specialty should Maisy Smith consider getting? Look for the species she gets the most. */
 SELECT s.name FROM specializations sp INNER JOIN species s ON s.id = sp.species_id 
 	GROUP BY s.name ORDER BY COUNT(*) DESC LIMIT 1
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
