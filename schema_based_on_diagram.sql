e4daffbe301b2d39488822aede23889ca06f387f
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-04-12 10:45:16 +0100

    add medical histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,11 @@ CREATE TABLE patients(
   date_of_birth DATE
 )
 
+CREATE TABLE medical_histories(
+  id INT PRIMARY KEY IDENTITY,
+  admitted_at TIMESTAMP WITHOUT TIME ZONE,
+  patient_id INT,
+  status VARCHAR(10),
+  Add CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id)
+)
+
