774f44cebcfd4b0ccf2a3d4f338053a1e4b6fe6e
jonas-45 <jonaswullah@gmail.com>
2023-04-12 09:50:32 +0000

    Add query to create indexes for tables in the clinic database"

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -47,3 +47,10 @@ CREATE TABLE history_treatments_join(
   ADD FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
   ADD FOREIGN KEY (treatments_id) REFERENCES treatments(id)
 )
+
+CREATE INDEX patient_id_idx ON medical_histories(patient_id);
+CREATE INDEX medical_history_id_idx ON invoices(medical_history_id);
+CREATE INDEX invoice_id_idx ON invoice_items(invoice_id);
+CREATE INDEX treatment_id_idx ON invoice_items(treatment_id);
+CREATE INDEX history_treatments_history_id_idx ON history_treatments_join(medical_history_id),
+CREATE INDEX history_treatments_treatment_id_idx ON history_treatments_join(treatments_id)
